e02a4189e755d5998fbdd3703cbc93db10d064c8
NabismoPrimeTime <00260006@student.necc.edu>
2020-04-10 17:20:53 -0400

    New method for extracting date and close price from cells

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import time
 
 rows_in_table = []
-dates_in_table = []
+cells_in_table = []
 sliced_close_price = []
 
 
@@ -20,39 +20,30 @@ def import_table():
     soup = bs4.BeautifulSoup(r.text, features="html.parser")
     table = soup.find('div', {'class': 'cmc-tab-historical-data ctxmt9-0 ASvFA'})
     rows = table.find_all('tr')
+
     global rows_in_table
+    global cells_in_table
 
     for row in rows:
-        rows_in_table.append(row.text)
-        print(row.text)
-
-def slice_dates_from_rows():
-    global dates_in_table
-    for date in rows_in_table:
-        sliced_date = date[0:12]
-        dates_in_table.append(sliced_date)
-
-def slice_close_price_from_rows():
-    global sliced_close_price
-    global rows_in_table
-    for close_price in rows_in_table:
-        sliced_close = close_price[36:44]
-        sliced_close_price.append(sliced_close)
-
-def list_cleaner(list_to_clean):
-    del list_to_clean[0:3]
+        if row != rows[0]:
+            rows_in_table.append(row.text)
+        #print(row.text)
+            for cell in row:
+                if cell.text != 'Date':
+                    cells_in_table.append(cell.text)
 
 
+def extract_close_prices(list_to_review):
+    global list_dates, list_close_price
+    list_dates = list_to_review[0:-1:7]
+    #print(list_dates)
+    list_close_price = list_to_review[4:-1:7]
+    #print(list_close_price)
+    return list_dates, list_close_price
 
 #expirementing how I want to break up the rows of data into appropriate strings
 import_table()
-slice_dates_from_rows()
-slice_close_price_from_rows()
-list_cleaner(dates_in_table)
-list_cleaner(sliced_close_price)
-
-print(dates_in_table)
-print(sliced_close_price)
-if 'Apr 07, 2020' in dates_in_table:
-    print('This is a valid date')
-
+#print(cells_in_table)
+extract_close_prices(cells_in_table)
+print(list_dates)
+print(list_close_price)
