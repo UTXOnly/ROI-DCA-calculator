724fe198ce868da245fdbc2d12eaff75a4998bb4
NabismoPrimeTime <00260006@student.necc.edu>
2020-04-10 08:58:31 -0400

    Added functions to data from table and remove unwanted elements

--- scraper.py
+++ scraper.py
@@ -3,7 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-list_of_dates = []
+rows_in_table = []
+dates_in_table = []
+sliced_close_price = []
+
 
 def parsePrice():
     r = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/')
@@ -17,20 +20,39 @@ def import_table():
     soup = bs4.BeautifulSoup(r.text, features="html.parser")
     table = soup.find('div', {'class': 'cmc-tab-historical-data ctxmt9-0 ASvFA'})
     rows = table.find_all('tr')
-    global list_of_dates
-
+    global rows_in_table
 
     for row in rows:
-        #print(row.text,' ')
-        #find_date = row.find_all('td', {'class': 'cmc-table__cell cmc-table__cell--sticky cmc-table__cell--left'})
-        list_of_dates.append(row.text)
+        rows_in_table.append(row.text)
         print(row.text)
 
-    return table
-#expiramenting how I want to break up the rows of data into appropriate strings
+def slice_dates_from_rows():
+    global dates_in_table
+    for date in rows_in_table:
+        sliced_date = date[0:12]
+        dates_in_table.append(sliced_date)
+
+def slice_close_price_from_rows():
+    global sliced_close_price
+    global rows_in_table
+    for close_price in rows_in_table:
+        sliced_close = close_price[36:44]
+        sliced_close_price.append(sliced_close)
+
+def list_cleaner(list_to_clean):
+    del list_to_clean[0:3]
+
+
+
+#expirementing how I want to break up the rows of data into appropriate strings
 import_table()
-print(list_of_dates)
-print(len(list_of_dates))
-seperated_dates = list_of_dates[3]#[2:5]
-print(seperated_dates)
+slice_dates_from_rows()
+slice_close_price_from_rows()
+list_cleaner(dates_in_table)
+list_cleaner(sliced_close_price)
+
+print(dates_in_table)
+print(sliced_close_price)
+if 'Apr 07, 2020' in dates_in_table:
+    print('This is a valid date')
 
